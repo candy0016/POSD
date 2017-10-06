e43105d92208e305d143ca5f13223fcfa573c926
candy0016 <32049477+candy0016@users.noreply.github.com>
2017-10-06 12:19:21 +0800

    Add files via upload

---
+++ mainAtom.cpp
@@ -0,0 +1,34 @@
+#include "atom.h"
+#include "variable.h"
+#include "number.h"
+
+#include <string>
+
+#include <iostream>
+using namespace std;
+
+bool Atom::match(Number *N){
+    return false;
+}
+
+
+bool Atom::match(Variable *V){
+    if(!assignFlag && V->get_assign()){
+        assignFlag = true;
+        V->setAptr(this);
+        V->set_assign_false();
+        return true;
+    }
+    else{
+        ptr1 = this;
+        ptr2 = V->getAptr();
+        //cout<<ptr1<<"----------";
+        //cout<<ptr2<<"----------";
+        if(ptr1 == ptr2) return true;
+        else return false;
+    }
+
+}
+
+
+
---
+++ variable.h
@@ -0,0 +1,46 @@
+#ifndef VARIABLE_H
+#define VARIABLE_H
+
+#include <string>
+
+using std::string;
+
+class Atom;
+class Number;
+
+class Variable{
+private:
+  string _value;
+  bool _assignable = true;
+  Atom *Aptr = nullptr;
+  Number *Nptr = nullptr;
+
+public:
+  Variable(string s):_symbol(s){}
+  string const _symbol;
+  string value(){ return _value; }
+  string symbol(){ return _symbol; }
+  void setAptr(Atom *A) { this->Aptr = A; }
+  void setNptr(Number *N) { this->Nptr = N; }
+  Atom *getAptr() { return this->Aptr; }
+  Number *getNptr() { return this->Nptr; }
+  void set_assign_false() { this->_assignable = false; }
+  bool get_assign() { return this->_assignable; }
+
+  bool match(Atom *A);
+  bool match(Number *N);
+
+  /*bool match( Atom atom ){
+    bool ret = _assignable;
+    if(_assignable){
+      _value = atom._symbol ;
+      _assignable = false;
+      atom._var = _symbol;
+    }
+    return ret;
+  }*/
+
+
+};
+
+#endif
