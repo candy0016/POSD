2456f326c73b714f1bb8d840c414bbe9cf487d43
candy0016 <32049477+candy0016@users.noreply.github.com>
2017-12-06 13:43:20 +0800

    Update atom.h

--- atom.h
+++ atom.h
@@ -2,71 +2,35 @@
 #define ATOM_H
 
 #include <string>
-#include <vector>
+#include <sstream>
+using std::string;
 
-using namespace std;
-
-
+template <typename T> class Iterator;
 class Term{
-
 public:
-    virtual string symbol() const = 0;
-    virtual string value() = 0;
-
-    virtual bool match(Term & term) = 0;
-    virtual int getTerm() = 0;
-    virtual bool get_assign() = 0;
-    virtual void set_assign() = 0;
-    virtual void setValue(string s) = 0;
-    virtual void setTemp(Term *t) = 0;
-    virtual vector<Term *> *getTemp() = 0;
-    virtual vector<Term *> *get_args() = 0;
-    virtual int arity() = 0;
-    virtual bool isContain(string symbol) { return false; }
-    virtual Term *findBySymbol(string symbol)
-    {
-      if (symbol == this->symbol())
-       return this;
-     else
-       return nullptr;
-    }
+  virtual string symbol() const {return _symbol;}
+  virtual string value() const {return symbol();}
+  virtual bool match(Term & a);
+  virtual Iterator<Term> * createIterator();
+protected:
+  Term ():_symbol(""){}
+  Term (string s):_symbol(s) {}
+  Term(double db){
+    std::ostringstream strs;
+    strs << db;
+    _symbol = strs.str();
+  }
+  string _symbol;
 };
 
 class Atom : public Term{
-
-private:
-  string _symbol,_value;
-  bool _assignable = false;
-
 public:
-  Atom (string s):_symbol(s), _value(s) {}
-
-  string symbol() const{
-    return _symbol;
-  }
-  string value() {
-    return _value;
-  }
-
-  bool match(Term & term) {
-      if(term.getTerm()==1 || term.getTerm()==2) { return symbol() == term.symbol(); }
-      else if(term.getTerm()==3){
-        term.match(*this);
-      }
-      else if(term.getTerm()==4){ return false; }
-      else if(term.getTerm()==5){ return false; }
-      else return false;
-    }
-
-  int getTerm() { return 1; }
-  bool get_assign() { return _assignable; }
-  void setValue(string s) { this->_symbol = s; }
-  void setTemp(Term *t) {}
-  vector<Term *> *getTemp() {}
-  vector<Term *> *get_args() {}
-  int arity(){}
-  void set_assign() {}
+  Atom(string s):Term(s) {}
 };
 
+class Number : public Term{
+public:
+  Number(double db):Term(db) {}
+};
 
 #endif
