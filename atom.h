04d56e12a41f32dcfa0f2ee62c5ccc0c1a2eb6c3
candy0016 <32049477+candy0016@users.noreply.github.com>
2017-12-07 18:52:11 +0800

    Update atom.h

--- atom.h
+++ atom.h
@@ -11,7 +11,7 @@ public:
   virtual string symbol() const {return _symbol;}
   virtual string value() const {return symbol();}
   virtual bool match(Term & a);
-  virtual Iterator<Term> * createIterator();
+  virtual Iterator<Term*> * createIterator();
 protected:
   Term ():_symbol(""){}
   Term (string s):_symbol(s) {}
