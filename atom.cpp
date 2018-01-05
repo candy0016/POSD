48a6845fa9cc907c17ebbebff992d4f8f759f7b7
candy0016 <32049477+candy0016@users.noreply.github.com>
2018-01-05 21:55:20 +0800

    Update atom.cpp

--- atom.cpp
+++ atom.cpp
@@ -6,10 +6,10 @@
 Iterator<Term *> * Term::createIterator(){
   return new NullIterator<Term *>(this);
 }
-
+/*
 bool Term::match(Term & a){
   if (typeid(a) ==  typeid(Variable))
     return a.match(*this);
   else
     return symbol() == a.symbol();
-}
+}*/
